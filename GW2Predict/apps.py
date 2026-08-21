5aecf33dd1fcf0230c43615288dbd67cd8799c41
Godfather76 <isaac.godsey@gmail.com>
2026-08-21 16:28:46 -0700

    fixed RUN_MAIN error

--- GW2Predict/apps.py
+++ GW2Predict/apps.py
@@ -9,21 +9,20 @@ class GW2PredictConfig(AppConfig):
     model_dict = {}
 
     def ready(self):
-        if environ.get('RUN_MAIN'):
-            from keras.models import load_model
-            models = ('penny', 'luxury')
-            days = ('3d', '7d', '30d')
-            path_base = path.join(settings.BASE_DIR, "GW2Predict", "models")
-            for model in models:
-                for day in days:
-                    this_model_path = path.join(path_base, f'{model}_model_{day}.keras')
-                    this_preprocess_path = path.join(path_base, f'{model}_preprocess_{day}.joblib')
-                    this_y_preprocess_path = path.join(path_base, f'{model}_y_preprocess_{day}.joblib')
-                    if path.exists(this_model_path):
-                        self.model_dict[f'{model}_{day}'] = {
-                            'model': load_model(this_model_path),
-                            'preprocess': joblib.load(this_preprocess_path),
-                            'y_preprocess': joblib.load(this_y_preprocess_path)
-                        }
+        from keras.models import load_model
+        models = ('penny', 'luxury')
+        days = ('3d', '7d', '30d')
+        path_base = path.join(settings.BASE_DIR, "GW2Predict", "models")
+        for model in models:
+            for day in days:
+                this_model_path = path.join(path_base, f'{model}_model_{day}.keras')
+                this_preprocess_path = path.join(path_base, f'{model}_preprocess_{day}.joblib')
+                this_y_preprocess_path = path.join(path_base, f'{model}_y_preprocess_{day}.joblib')
+                if path.exists(this_model_path):
+                    self.model_dict[f'{model}_{day}'] = {
+                        'model': load_model(this_model_path),
+                        'preprocess': joblib.load(this_preprocess_path),
+                        'y_preprocess': joblib.load(this_y_preprocess_path)
+                    }
 
-            print("--- Keras models successfully cached into Django memory ---")
+        print("--- Keras models successfully cached into Django memory ---")
